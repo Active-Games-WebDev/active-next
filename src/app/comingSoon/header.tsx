ad07b8dcc4542534c413095c363f6c70f18d64e4
ActiveGamesDevTeam <activegamescl@gmail.com>
2024-05-30 23:52:34 -0400

    comingSoon responsive

--- src/app/comingSoon/header.tsx
+++ src/app/comingSoon/header.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import Header from "./header/header";
-import ComingSoon from "./comingSoon/header";
-
-
+import Image from "next/image";
 
 export default function App() {
   return (
@@ -31,10 +28,16 @@ export default function App() {
           <link href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/grids-responsive-min.css" rel="stylesheet"/>
           {/*pure grids responsive*/}
 
-
       </head>
+
       <body>
-        <ComingSoon/>
+      {/*header*/}
+
+          <div className="comingSoon" id="comingSoon">
+              <link href="./stylesheets/comingSoon.css" rel="stylesheet"/>
+              <img className="pageHeaderIcon1080" src="/resources/active-proximamente-@1080.png" alt="active-comingSoon"/>
+              <img className="pageHeaderIconPhone" src="/resources/active-proximamente-phone-size.png" alt="active-comingSoon"/>
+          </div>
 
       </body>
       </html>
